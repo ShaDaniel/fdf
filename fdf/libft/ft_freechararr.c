e3d0fb5c8026375a755328d1ccb0de93649fe61b
Egor <arara1196@gmail.com>
2020-05-13 21:20:06 +0300

    parse leak test

--- fdf/libft/ft_freechararr.c
+++ fdf/libft/ft_freechararr.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/20 21:10:49 by tjonella          #+#    #+#             */
-/*   Updated: 2020/05/13 21:10:39 by marvin           ###   ########.fr       */
+/*   Updated: 2020/05/13 21:19:49 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ void	ft_freechararr(char **arr)
 	{
 		ft_putnbr(i);
 		ft_putchar('\n');
-		free(arr[i++]);
+		free(arr[i]);
+		arr[i] = NULL;
+		i++;
 	}
 	free(arr);
+	arr = NULL;
 }
