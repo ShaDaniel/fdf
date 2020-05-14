a50760404ae734163bd1b3cc06f4de53c6ed20cc
Egor <arara1196@gmail.com>
2020-05-14 05:09:06 +0300

    gradient test

--- fdf/fdf_draw_img.c
+++ fdf/fdf_draw_img.c
@@ -8,7 +8,7 @@ static void	fdf_point_set(t_point *p, t_point *fin, size_t x, size_t y, t_main *
 	p->x = fdf->offset->x + x * DIST_MIN * fdf->map->zoom;
 	p->y = fdf->offset->y + y * DIST_MIN * fdf->map->zoom;
 	p->z = fdf->map->coords[y * fdf->map->width + x] * fdf->map->zscale;
-	p->colour_s = (int)fdf->map->colours[y * fdf->map->width + x];
+	p->colour_s = fdf->map->colours[y * fdf->map->width + x] & 0xFFFFFF;
 	if (p->colour_s == 0)
 		p->colour_s = WHITE;
 	ft_putnbr(p->colour_s);
