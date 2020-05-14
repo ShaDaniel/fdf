e45a812e8cda1a437470919da080d17abc2ab650
Egor <arara1196@gmail.com>
2020-05-15 01:24:25 +0300

    key hook continue

--- fdf/fdf_hooks.c
+++ fdf/fdf_hooks.c
@@ -12,26 +12,26 @@ static void	arrows_move(t_main *fdf, int keycode)
 		fdf->offset->x += 10;
 }
 
-int			keyboard_hook(int keycode, void *param)
+int			keyboard_hook(int key, void *param)
 {
 	t_main *fdf;
 
 	fdf = (t_main *)param;
-	if (keycode == KEY_ESC)
+	if (key == KEY_ESC)
 		exit(0);
-	else if (keycode == KEY_I)
+	else if (key == KEY_I)
 		fdf->iso = 1;
-	else if (keycode == KEY_P)
+	else if (key == KEY_P)
 		fdf->iso = 0;
-	else if (keycode == KEY_DOWN || KEY_UP || KEY_RIGHT || KEY_LEFT)
-		arrows_move(fdf, keycode);
-	else if (keycode == KEY_LBRK && fdf->map->zoom > 1)
+	else if (key == KEY_DOWN || key == KEY_UP || key == KEY_RIGHT || key == KEY_LEFT)
+		arrows_move(fdf, key);
+	else if (key == KEY_LBRK && fdf->map->zoom > 1)
 		fdf->map->zoom--;
-	else if (keycode == KEY_RBRK && fdf->map->zoom < 40)
+	else if (key == KEY_RBRK && fdf->map->zoom < 40)
 		fdf->map->zoom++;
-	else if (keycode == KEY_Z && fdf->map->zscale < 20)
+	else if (key == KEY_Z && fdf->map->zscale < 20)
 		fdf->map->zscale++;
-	else if (keycode == KEY_X && fdf->map->zscale > -20)
+	else if (key == KEY_X && fdf->map->zscale > -20)
 		fdf->map->zscale--;
 	else
 		return (0);
