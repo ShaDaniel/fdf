1064f6c66d2d4c1db6464c20ad44c30bf241eb94
Egor <arara1196@gmail.com>
2020-05-14 05:23:49 +0300

    gradient test

--- fdf/fdf_draw_img.c
+++ fdf/fdf_draw_img.c
@@ -11,6 +11,8 @@ static void	fdf_point_set(t_point *p, t_point *fin, size_t x, size_t y, t_main *
 	p->colour_s = fdf->map->colours[y * fdf->map->width + x] & 0xFFFFFF;
 	if (x == 0 && y == 0)
 		ft_putnbr(p->colour_s);
+	if (!p->colour_s)
+		p->colour_s = WHITE;
 	if (fin)
 		p->colour_f = fin->colour_s;
 	if (fdf->iso)
